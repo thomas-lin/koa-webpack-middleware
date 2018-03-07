edbd432c5bb3bc908fbe4b48a239847c04feecca
thomas <bbkings2002@gmail.com>
2018-03-07 13:12:20 +0800

    fix request get octet-stream

--- middleware/hotMiddleware.js
+++ middleware/hotMiddleware.js
@@ -5,12 +5,12 @@ export default (compiler, opts) => {
   const expressMiddleware = hotMiddleware(compiler, opts)
   return async (ctx, next) => {
     let stream = new PassThrough()
-    ctx.body = stream
     await expressMiddleware(ctx.req, {
       write: stream.write.bind(stream),
       writeHead: (status, headers) => {
+        ctx.body = stream
         ctx.status = status
-        ctx.set(headers)
+        ctx.set(headers)       
       }
     }, next)
   }
