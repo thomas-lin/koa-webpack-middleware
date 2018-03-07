ee9a07a4ac62a5b7735ec3eafd99a0acb8d249a5
Thomas <bbkings2002@gmail.com>
2018-03-07 14:37:32 +0800

    add hodMiddleware bug refs

--- middleware/hotMiddleware.js
+++ middleware/hotMiddleware.js
@@ -1,6 +1,12 @@
 import hotMiddleware from 'webpack-hot-middleware'
 import { PassThrough } from 'stream'
 
+/*
+    @Override koa-webpack-middleware/hotMiddleware
+    fix application/octet-stream issue for any middleware after the hot middleware
+    which causes Chrome to prompt for download.
+    refs: https://github.com/leecade/koa-webpack-middleware/issues/23
+*/
 export default (compiler, opts) => {
   const expressMiddleware = hotMiddleware(compiler, opts)
   return async (ctx, next) => {
